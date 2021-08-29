b4b29a2f8d171daac8fd57bc19b8a81c133f3289
Emmerz6949 <emmerz6949@gmail.com>
2021-08-29 18:10:25 -0400

    removed a problematic import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
