ae3a7af2b4dc25d5ad98bb12a55cac1d44de4918
Emmerz6949 <emmerz6949@gmail.com>
2021-08-29 15:42:13 -0400

    added the 'components' folder and the Header.js file inside with some base code

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+
+function Header(props) {
+    const location = useLocation();
+
+    return(
+        <header>
+
+        </header>
+    );
+}
+
+export default Header;
