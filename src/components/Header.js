067c6d647ec832ca093c3fd4830604dd1376eb3b
Emmerz6949 <emmerz6949@gmail.com>
2021-08-29 18:08:26 -0400

    updated Header.js to be Bootstrap 5 compatible

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,18 @@ function Header(props) {
     return(
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-opacity-75">
-                <h1 className="navbar-brand mb-0"><Link to="/hackathon-covlab-ii-2021/" className="text-decoration-none text-reset">Science-rific Labs</Link></h1>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <Link to="/hackathon-covlab-ii-2021/" className={location.pathname === "/hackathon-covlab-ii-2021/" ? "nav-link active" : "nav-link"}>Home</Link>
-                        </li>
-                    </ul>
+                <div className="container-fluid">
+                    <h1 className="navbar-brand mb-0"><Link to="/hackathon-covlab-ii-2021/" className="text-decoration-none text-reset">Science-rific Labs</Link></h1>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <Link to="/hackathon-covlab-ii-2021/" className={location.pathname === "/hackathon-covlab-ii-2021/" ? "nav-link active" : "nav-link"}>Home</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
