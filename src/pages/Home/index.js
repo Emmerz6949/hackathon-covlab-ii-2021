c0a21791546aa20008cc96bdb86b2243a931e312
Emmerz6949 <emmerz6949@gmail.com>
2021-08-30 23:02:01 -0400

    added some styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Sci from "./sci/science.png";
 
+const styles = {
+    cardStyle: {
+        margin: "0px auto",
+        width: "50%"
+    }
+};
+
 function Home() {
     return(
         <main>
-            <div className="card">
+            <div className="card border-0" style={styles.cardStyle}>
                 <img src={Sci} className="card-img-top" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
