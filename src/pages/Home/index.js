82fd26dd525608a956196b6a8abe56ec788e259d
Emmerz6949 <emmerz6949@gmail.com>
2021-08-30 22:24:59 -0400

    added a bootstrap card and updated the img src to the Sci import

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,12 @@ import Sci from "./sci/science.png";
 function Home() {
     return(
         <main>
-
+            <div className="card">
+                <img src={Sci} className="card-img-top" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </main>
     );
 }
