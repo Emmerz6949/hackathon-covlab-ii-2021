29e3638f393d29a4e9bccc6f5d6365a6288b3882
Emmerz6949 <emmerz6949@gmail.com>
2021-08-31 01:27:48 -0400

    updated the navbar classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
 
     return(
         <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-opacity-75">
+            <nav className="navbar navbar-expand-lg navbar-light bg">
                 <div className="container-fluid">
                     <h1 className="navbar-brand mb-0"><Link to="/hackathon-covlab-ii-2021/" className="text-decoration-none text-reset">Science-rific Labs</Link></h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
