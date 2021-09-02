4caac89192608a041ebb97f5c120ba976f3dd967
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 03:01:21 -0400

    added 3 Options to the Choose page

--- src/pages/Choose/index.js
+++ src/pages/Choose/index.js
@@ -5,6 +5,9 @@ function Choose() {
     return(
         <main>
             <h2 className="border border-dark border-3 rounded-pill bg mt-5">Choose Your Science</h2>
+            <Option id="1" key="1" link="chemistry" text="Chemistry"/>
+            <Option id="2" key="2" link="biology" text="Biology"/>
+            <Option id="3" key="3" link="physics" text="Physics"/>
         </main>
     );
 }
