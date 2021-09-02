7a0e850445a6b129490e057666bcc37459e27db9
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 01:28:06 -0400

    connected the 'Let's Get Started' button to the Choose page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,7 +16,7 @@ function Home() {
             <div id="home" className="card border-0" style={styles.cardStyle}>
                 <img src={Sci} className="card-img-top" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
                 <div className="card-body pt-0">
-                    <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg">Let's Get Started!</h2>
+                    <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg"><Link to="/hackathon-covlab-ii-2021/choose" className="text-decoration-none text-reset">Let's Get Started!</Link></h2>
                 </div>
             </div>
         </main>
