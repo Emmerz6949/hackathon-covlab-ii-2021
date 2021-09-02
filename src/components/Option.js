2a9ae777095a045a794a9b391ed8975480054dca
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 04:00:33 -0400

    updated the placeement of props.link

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 function Option(props) {
-    let toDest = `"/hackathon-covlab-ii-2021/${props.link}"`;
-
     return(
-        <h4 className=" btn fs-4 border-dark border-3 rounded-pill bg mt-5"><Link to={toDest} className="text-decoration-none text-reset">{props.text}</Link></h4>
+        <h4 className=" btn fs-4 border-dark border-3 rounded-pill bg mt-5"><Link to={props.link} className="text-decoration-none text-reset">{props.text}</Link></h4>
     );
 }
 
