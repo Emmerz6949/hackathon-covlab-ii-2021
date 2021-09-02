591dc1bf35c569787d7d0ac54c052a674046f384
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 04:17:04 -0400

    updated the Option function so that the h4 button is wrapped in a div

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 function Option(props) {
     return(
-        <h4 className=" btn fs-4 border-dark border-3 rounded-pill bg mt-5"><Link to={props.link} className="text-decoration-none text-reset">{props.text}</Link></h4>
+        <div>
+            <h4 className=" btn fs-4 border-dark border-3 rounded-pill bg mx-auto mt-5"><Link to={props.link} className="text-decoration-none text-reset">{props.text}</Link></h4>
+        </div>
     );
 }
 
