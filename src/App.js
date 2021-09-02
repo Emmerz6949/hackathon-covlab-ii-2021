677d0c41ece3c1e34f1dd2b02a0c0671bb1d4d0d
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 01:41:18 -0400

    added the Choose page as a route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Choose from './pages/Choose';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Header />
         <Route exact path="/hackathon-covlab-ii-2021/" component={Home} />
+        <Route exact path="/hackathon-covlab-ii-2021/choose" component={Choose} />
       </div>
     </Router>
   );
