5f29a5c7fd83119612d3587429e8e10a6523ad27
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 03:30:25 -0400

    updated the Choose function to a class and extended the Component class

--- src/pages/Choose/index.js
+++ src/pages/Choose/index.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import Option from "../../components/Option";
 import options from "../../options.json";
 
-function Choose() {
-    return(
-        <main>
-            <h2 className="border border-dark border-3 rounded-pill bg mt-5">Choose Your Science</h2>
-            <Option id="1" key="1" link="chemistry" text="Chemistry"/>
-            <Option id="2" key="2" link="biology" text="Biology"/>
-            <Option id="3" key="3" link="physics" text="Physics"/>
-        </main>
-    );
+class Choose extends Component {
+    render() {
+        return(
+            <main>
+                <h2 className="border border-dark border-3 rounded-pill bg mt-5">Choose Your Science</h2>
+                <Option id="1" key="1" link="chemistry" text="Chemistry"/>
+                <Option id="2" key="2" link="biology" text="Biology"/>
+                <Option id="3" key="3" link="physics" text="Physics"/>
+            </main>
+        );
+    }
 }
 
 export default Choose;
