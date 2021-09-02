16ad31b72440b5890501535477e1d02a53da6a6d
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 02:11:26 -0400

    added an h2 to the Choose page

--- src/pages/Choose/index.js
+++ src/pages/Choose/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Choose() {
     return(
         <main>
-
+            <h2 className="border border-dark border-3 rounded-pill bg mt-5">Choose Your Science</h2>
         </main>
     );
 }
