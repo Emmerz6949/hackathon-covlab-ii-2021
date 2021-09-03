a1b78bec1d1f73ad70ea993d56b338663c0928c2
Emmerz6949 <emmerz6949@gmail.com>
2021-09-03 02:14:52 -0400

    added another paragraph to the Chem page

--- src/pages/Chem/index.js
+++ src/pages/Chem/index.js
@@ -20,7 +20,10 @@ function Chem() {
                     This expermint utilizes a hot plate, and a solution made up of 500ml of water, a drop of concentrated ammonia,
                      and a few drops of phenolphthalein indicator. At room temperature the solution will be pink due to the pH of the concentrated ammonia.
                 </p>
-                <img src={Pink} className="pb-5 bg" style={styles.imgStyle} alt="a flask containing a pink solution" />
+                <img src={Pink} className="pb-3 bg" style={styles.imgStyle} alt="a flask containing a pink solution" />
+                <p className="fw-bold">
+                    Now let's try heating the solution!
+                </p>
             </div>
         </main>
     );
