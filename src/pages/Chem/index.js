846ac8f975312ef248c01b8620e88a7baa26384c
Emmerz6949 <emmerz6949@gmail.com>
2021-09-03 01:00:03 -0400

    added a paragraph to the index.js of the 'Chem' folder

--- src/pages/Chem/index.js
+++ src/pages/Chem/index.js
@@ -11,6 +11,10 @@ function Chem() {
         <main>
             <div className="mx-auto mt-5 bg" style={styles.divStyle}>
                 <h2 className="text-decoration-underline">Chemistry Experiment</h2>
+                <p className="fw-bold">
+                    This expermint utilizes a hot plate, and a solution made up of 500ml of water, a drop of concentrated ammonia,
+                     and a few drops of phenolphthalein indicator. At room temperature the solution will be pink due to pH of the concentrated ammonia.
+                </p>
             </div>
         </main>
     );
