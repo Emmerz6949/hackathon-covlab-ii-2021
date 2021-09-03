3322ca28e366c6247f562e036d78ea9b19d290c2
Emmerz6949 <emmerz6949@gmail.com>
2021-09-02 23:48:59 -0400

    added and stylized an h2 in the index.js of the 'Chem' folder

--- src/pages/Chem/index.js
+++ src/pages/Chem/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const styles = {
+    divStyle: {
+        width: "50%"
+    }
+};
+
 function Chem() {
     return(
         <main>
-
+            <div className="mx-auto mt-5 bg" style={styles.divStyle}>
+                <h2 className="text-decoration-underline">Chemistry Experiment</h2>
+            </div>
         </main>
     );
 }
