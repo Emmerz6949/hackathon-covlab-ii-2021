d7a6e020444959bec01c7f12f0440db6f120f419
Emmerz6949 <emmerz6949@gmail.com>
2021-09-03 01:57:41 -0400

    added and stylized an img to index.js in the 'Chem' folder

--- src/pages/Chem/index.js
+++ src/pages/Chem/index.js
@@ -5,6 +5,9 @@ import Clear from './flasks/clearSolutionFlask.png';
 const styles = {
     divStyle: {
         width: "50%"
+    },
+    imgStyle: {
+        width: "15%"
     }
 };
 
@@ -17,6 +20,7 @@ function Chem() {
                     This expermint utilizes a hot plate, and a solution made up of 500ml of water, a drop of concentrated ammonia,
                      and a few drops of phenolphthalein indicator. At room temperature the solution will be pink due to pH of the concentrated ammonia.
                 </p>
+                <img src={Pink} className="pb-5 bg" style={styles.imgStyle} alt="a flask containing a pink solution" />
             </div>
         </main>
     );
