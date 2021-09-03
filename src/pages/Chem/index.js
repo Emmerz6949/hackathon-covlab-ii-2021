8cc8f4e7cf6b188228cb1d32d9551db43d9a2e44
Emmerz6949 <emmerz6949@gmail.com>
2021-09-03 02:00:20 -0400

    added a missing word to the paragraph in the index.js of the 'Chem' folder

--- src/pages/Chem/index.js
+++ src/pages/Chem/index.js
@@ -18,7 +18,7 @@ function Chem() {
                 <h2 className="text-decoration-underline">Chemistry Experiment</h2>
                 <p className="fw-bold">
                     This expermint utilizes a hot plate, and a solution made up of 500ml of water, a drop of concentrated ammonia,
-                     and a few drops of phenolphthalein indicator. At room temperature the solution will be pink due to pH of the concentrated ammonia.
+                     and a few drops of phenolphthalein indicator. At room temperature the solution will be pink due to the pH of the concentrated ammonia.
                 </p>
                 <img src={Pink} className="pb-5 bg" style={styles.imgStyle} alt="a flask containing a pink solution" />
             </div>
