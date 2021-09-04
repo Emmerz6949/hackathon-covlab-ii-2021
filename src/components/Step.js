2147f93846c5619899342e98087cbad23f845263
Emmerz6949 <emmerz6949@gmail.com>
2021-09-04 02:19:17 -0400

    added an img, a p, and an h5 classed as a button inside the div of Step.js

--- src/components/Step.js
+++ src/components/Step.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function Step(props) {
     return(
-        <div></div>
+        <div>
+            <img src={props.imgSrc} className="pb-3 bg" style={props.imgStylin} alt={props.altText} />
+            <p className="fw-bold">
+                {props.pText}
+            </p>
+            <h5 className="btn fs-5 border-dark border-3 rounded-pill bg">{props.btnText}</h5>
+        </div>
     );
 }
 
