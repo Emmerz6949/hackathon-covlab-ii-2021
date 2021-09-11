83606376ebe7af59ebe0db07f7f6ca30463eab3c
Emmerz6949 <emmerz6949@gmail.com>
2021-09-11 03:37:07 -0400

    imported Link from react-router-dom

--- src/pages/ResultsChem/index.js
+++ src/pages/ResultsChem/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Pink from '../Chem/flasks/pinkSolutionFlask.png';
 import Clear from '../Chem/flasks/clearSolutionFlask.png';
 import 'bootstrap-icons/font/bootstrap-icons.css';
