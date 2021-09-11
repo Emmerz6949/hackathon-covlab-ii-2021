abe16a47a5fe094ac59935ed883fc2cfd0f778e8
Emmerz6949 <emmerz6949@gmail.com>
2021-09-11 01:16:16 -0400

    added a styling class to the img on the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ function Home() {
     return(
         <main>
             <div id="home" className="card border-0" style={styles.cardStyle}>
-                <img src={Sci} className="card-img-top" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
+                <img src={Sci} className="card-img-top text-light" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
                 <div className="card-body pt-0">
                     <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg"><Link to="/hackathon-covlab-ii-2021/choose" className="text-decoration-none text-reset">Let's Get Started!</Link></h2>
                 </div>
