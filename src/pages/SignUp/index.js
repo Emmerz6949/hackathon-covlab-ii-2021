b21e0f34013b538b12291879c7ac3136dda44870
Emmerz6949 <emmerz6949@gmail.com>
2021-10-24 04:41:17 -0400

    added a div containing an h2, a form with 2 input fields with labels and an h5 classed as a button, and two h5 with one classed as a button, also added some styling

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const styles = {
+    divStyle: {
+        width: "50%"
+    },
+    inputStyle: {
+        width: "75%"
+    }
+};
+
 function SignUp() {
     return(
         <main>
-            
+            <div className="mx-auto mt-5 bg" style={styles.divStyle}>
+                <h2 className="text-decoration-underline">Sign Up</h2>
+                <form className="signup">
+                    <div className="form-group">
+                        <label for="email-input" className="fs-5">Email Address</label>
+                        <input type="email" className="form-control mx-auto" id="email-input" placeholder="Email" style={styles.inputStyle}/>
+                    </div>
+                    <div className="form-group">
+                        <label for="password-input" className="fs-5">Password</label>
+                        <input type="password" className="form-control mx-auto" id="password-input" placeholder="Password" style={styles.inputStyle}/>
+                    </div>
+                    <h5 className="btn fs-5 border-dark border-3 rounded-pill bg mt-3">Sign Up</h5>
+                </form>
+                <h5>or</h5>
+                <h5 className="btn fs-5 border-dark border-3 rounded-pill bg">Login</h5>
+            </div>
         </main>
     );
 }
