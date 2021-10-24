799af88aec2b0518c39ac3caeb8b2a5595116546
Emmerz6949 <emmerz6949@gmail.com>
2021-10-24 05:40:00 -0400

    added a 'Login' h2 classed as abutton to the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -17,6 +17,7 @@ function Home() {
                 <img src={Sci} className="card-img-top text-light" alt="a blue oval containing the underlined words 'Science-rific Labs'"/>
                 <div className="card-body pt-0">
                     <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg me-2"><Link to="/science-rific-labs/sign-up" className="text-decoration-none text-reset">Sign Up</Link></h2>
+                    <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg me-2"><Link to="/science-rific-labs/login" className="text-decoration-none text-reset">Login</Link></h2>
                     <h2 className="card-title btn fs-2 border-dark border-3 rounded-pill bg"><Link to="/science-rific-labs/choose" className="text-decoration-none text-reset">Let's Get Started!</Link></h2>
                 </div>
             </div>
